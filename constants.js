30465b71dd3a159b6e551831bef5ad08c0c09d81
John Paul M. Batusan <johnpaul.batusa@bicol-u.edu.ph>
2022-12-15 11:12:17 +0800

    Update constants

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 // Replace the value with your NFT contract address
 const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
-  "0x74757EA27657a202cF62c1adFb3d994F80070053";
+  "0x02ae939cf29484390233d43d88d4d786da19190c";
 
 module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
