7b9e469390624a79845fb57a9cc82ea8f733eabd
John Paul M. Batusan <johnpaul.batusa@bicol-u.edu.ph>
2022-12-15 10:40:16 +0800

    Add initial dao contract files

---
+++ constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x74757EA27657a202cF62c1adFb3d994F80070053";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
